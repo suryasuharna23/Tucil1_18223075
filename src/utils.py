607bb7af23f4fce49dc1c895a02c0c813b4a20b8
Surya Suharna <19623125@std.stei.itb.ac.id>
2026-02-13 23:10:28 +0700

    feat: enhance isValid function and add move and getColor functions

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ def isValid(board, row, col, colored_area):
     for i in range(len(board)):
         if (board[row][i] == 1) and (i != col):
             return False
+        if (board[i][col] == 1) and (i != row):
+            return False
 
     # Check diagonals
     for i in range(len(board)):
@@ -20,3 +22,31 @@ def isValid(board, row, col, colored_area):
             return False
     
     return True
+
+def move(board, row, colored_area):
+    n = len(board)
+
+    if row >= n:
+        return True
+    for col in range(n):
+        if isValid(board, row, col, colored_area):
+            board[row][col] = 1
+            if move(board, row+1, colored_area):
+                return True
+            board[row][col] = 0
+    return False
+
+def getColor(area):
+    rows = area.strip().split('\n')
+    n = len(rows)
+    areas = {}
+
+    for i in range(n):
+        rc = rows[i].strip()
+        for j in range(n):
+            color = rc[j]
+            if color not in areas:
+                areas[color] = []
+            areas[color].append((i,j))
+
+    return list(areas.values())
