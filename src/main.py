e7a7721fc25c6349d1ee58b78dc68393b85e98c1
Surya Suharna <19623125@std.stei.itb.ac.id>
2026-02-14 01:49:41 +0700

    fix: add main.py fetching .txt and testing block coordinates

--- src/main.py
+++ src/main.py
@@ -1,2 +1,23 @@
+from utils import *
+
 def main():
-	pass
+    n = int(input("Masukkan n: "))
+    filename = input("Masukkan nama file: ")
+    with open(f"../test/input/{filename}.txt", 'r') as f:
+        content = f.read()
+    
+    if n != len(content.strip().split('\n')):
+        print("Input tidak sesuai dengan n, silakan cek lagi input")
+        return
+
+    colored_content = loadFile(filename)
+    print(colored_content)
+    print('\n')
+
+    mapColor = getColorData(content)
+    print(mapColor)
+
+    board = initial_board(n)
+
+if __name__ == "__main__":
+    main()
--- src/utils.py
+++ src/utils.py
@@ -36,17 +36,77 @@ def move(board, row, colored_area):
             board[row][col] = 0
     return False
 
-def getColor(area):
+def getColorData(area):
     rows = area.strip().split('\n')
     n = len(rows)
     areas = {}
 
     for i in range(n):
-        rc = rows[i].strip()
-        for j in range(n):
+        rc = rows[i].strip() # rc = color in row i
+        for j in range(len(rc)):
             color = rc[j]
             if color not in areas:
                 areas[color] = []
             areas[color].append((i,j))
 
-    return list(areas.values())
+    return areas
+
+def loadFile(filename):
+    with open(f"../test/input/{filename}.txt", 'r') as f:
+        content = f.read()
+    
+    colors = [
+        "\033[91m", "\033[92m", 
+        "\033[93m", "\033[94m", 
+        "\033[95m", "\033[96m",
+        "\033[31m", "\033[32m", 
+        "\033[33m", "\033[34m", 
+        "\033[35m", "\033[36m",
+        "\033[91;1m", "\033[92;1m", 
+        "\033[93;1m", "\033[94;1m", 
+        "\033[95;1m", "\033[96;1m", 
+        "\033[37m", "\033[90m", 
+        "\033[41m", "\033[42m", 
+        "\033[43m", "\033[44m", 
+        "\033[45m", "\033[46m"
+    ]
+    reset = "\033[0m"
+
+    unique_chars = []
+
+    for i in range(len(content)):
+        char = content[i]
+
+        if char == '\n':
+            continue
+        if char == ' ':
+            continue
+
+        exist = False
+        for j in range(len(unique_chars)):
+            if unique_chars[j] == char:
+                exist = True
+                break
+        
+        if exist == False:
+            unique_chars.append(char)
+
+        r = ""
+        for i in range(len(content)):
+            char = content[i]
+            if char == '\n' or char == ' ':
+                r += char
+                continue
+            
+            idx = 0
+            for j in range(len(unique_chars)):
+                if unique_chars[j] == char:
+                    idx = j
+                    break
+            while idx >= len(colors):
+                idx -= len(colors)
+
+            selected = colors[idx]
+
+            r += selected + char + reset
+    return r
